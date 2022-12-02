26e7a10685c935b55d5141bd72557c7fa2fa0f94
John Edward Binay <qjeabinay@tip.edu.ph>
2022-12-02 09:16:28 +0000

    cards are now only clickable once delete button has been clicked. Clicking the cards during delete stores their id in an array. Clicking the card again deletes it from the array.

--- src/Card.js
+++ src/Card.js
@@ -5,15 +5,28 @@ import { useState } from "react";
 import "tachyons";
 // path to picture cannot be outsode of src?
 
-const Card = ({ name, email, id }) => {
+const Card = ({ name, email, id, deleteMode }) => {
   // const { name, email, id } = props;
   const [clicked, setClicked] = useState(false);
- 
+  const [clcikedCourses, setClickedCourses] = useState([]);
+
+  const onClicked = (id) => {
+    if (deleteMode) {
+      setClicked(!clicked);
+      if (clcikedCourses.includes(id)) {
+        setClickedCourses(clcikedCourses.filter((course) => course !== id));
+      } else {
+        setClickedCourses([...clcikedCourses, id]);
+      }
+    }
+    console.log(clcikedCourses);
+  };
+
   return (
     <div
-      onClick={() => setClicked(!clicked)}
+      {...(deleteMode ? { onClick: () => onClicked(id) } : {})}
       className="pointer tc bg-light-green dib br3 ma2 bw2 shadow-5 dim"
-      {...(clicked ? { style: { border: "5px solid red" } } : {})}  
+      {...(clicked && deleteMode ? { style: { border: "5px solid red" } } : {})}
     >
       {/* css to add for when clicked: b--dark-red br2 dim-on-click */}
       <img
--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,38 +1,38 @@
 import React from "react";
 import Card from "../../Card";
 import "tachyons";
-import {useState} from 'react';
+// import { useState } from "react";
 
 const CardList = ({ robots, deleteMode }) => {
+  // const [clcikedCourses, setClickedCourses] = useState([]);
 
-	const [clcikedCourses, setClickedCourses] = useState([]);
+  // const onClick = (id) => {
+  // 	if (deleteMode) {
+  // 		if (clcikedCourses.includes(id)) {
+  // 			setClickedCourses(clcikedCourses.filter((course) => course !== id));
+  // 		} else {
+  // 			setClickedCourses([...clcikedCourses, id]);
+  // 		}
+  // 	}
+  // 	console.log(clcikedCourses);
+  // };
 
-	// const onClick = (id) => {
-	// 	if (deleteMode) {
-	// 		if (clcikedCourses.includes(id)) {
-	// 			setClickedCourses(clcikedCourses.filter((course) => course !== id));
-	// 		} else {
-	// 			setClickedCourses([...clcikedCourses, id]);
-	// 		}
-	// 	}
-	// 	console.log(clcikedCourses);
-	// };
-
-	return (
-		<div className="dtc">
-			{robots.map((user) => {
-				return (
-					<Card
-						key={user.id}
-						id={user.id}
-						name={user.course}
-						email={user.email}
-						// onClick={onClick(user.id)}
-					/>
-				);
-			})}
-		</div>
-	);
+  return (
+    <div className="dtc">
+      {robots.map((user) => {
+        return (
+          <Card
+            deleteMode={deleteMode}
+            key={user.id}
+            id={user.id}
+            name={user.course}
+            email={user.email}
+            // onClick={onClick(user.id)}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default CardList;
