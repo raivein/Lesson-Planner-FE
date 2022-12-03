befc4e825d67265c75a03ebe2dcc1f427f7755a6
John Edward Binay <qjeabinay@tip.edu.ph>
2022-12-03 05:20:07 +0000

    modified  src/Card.js so that when deleteMode is false, the clicked attribute goes back to false for all the cards that were clicked. In this way, the red broder around them disappears

--- src/Card.js
+++ src/Card.js
@@ -16,22 +16,25 @@ const Card = ({ name, email, id, deleteMode, ClickedCourses }) => {
   }
 
   useEffect(() => {
-    console.log(clicked, id);
+    console.log("button clicked? ",clicked, id);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [clicked]);
 
-  
+    useEffect(() => {
+      console.log("class deleteMode", deleteMode);
+      setClicked(false);
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [deleteMode]);
+
+
 
   return (
     <div
       {...(deleteMode ? { onClick: () => onClicked(id) } : {})}
       // className="pointer tc bg-light-green dib ma2 shadow-5 dim"
       // {...(clicked && deleteMode ? { style: { border: "5px solid red" } } : {})}
-      className={
-        clicked && deleteMode
-          ? "pointer tc bg-light-green dib ma2 shadow-5 dim ba bw2 b--dark-red"
-          : "pointer tc bg-light-green dib ma2 shadow-5 dim"
-      }
+      className="pointer tc bg-light-green dib ma2 shadow-5 dim"
+      {...(deleteMode ? clicked ? { style: { border: "5px solid red" } } : {} : {})}
     >
       {/* css to add for when clicked: b--dark-red br2 dim-on-click */}
       <img
