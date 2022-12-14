7e26be00be5e86de58d143cefdeefc6d0dcaecef
John Edward Binay <qjeabinay@tip.edu.ph>
2022-12-14 15:44:13 +0000

    changed the table

--- src/components/Planner/Table/Table.js
+++ src/components/Planner/Table/Table.js
@@ -34,9 +34,17 @@ const StyledTableRow = styled(TableRow)(({ theme }) => ({
   },
 }));
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(topics, ILOs, TLAs, ATs, Remarks) {
+  return { topics, ILOs, TLAs, ATs, Remarks };
 }
+
+// const topics = ["topic 1", "topic 2", "topic 3", "topic 4", "topic 5"];
+// const ILOs = ["ILO 3", "ILO 4", "ILO 5"];
+// const TLAs = ["TLA 1", "TLA 2", "TLA 4", "TLA 5"];
+// const ATs = ["AT 1", "AT 2", "AT 3", "AT 4", "AT 5"];
+// const Remarks = ["Remarks 1", "Remarks 2", "Remarks 3"];
+
+// const rows = [topics, ILOs, TLAs, ATs, Remarks];
 
 const rows = [
   createData(
@@ -53,6 +61,7 @@ const rows = [
 ];
 
 export default function CustomizedTables() {
+  console.log("rows", rows);
   return (
     <div className="ma3">
       <TableContainer component={Paper}>
@@ -121,25 +130,101 @@ export default function CustomizedTables() {
               </StyledTableCell>
             </TableRow>
           </TableHead>
-          <TableBody>
-            {rows.map((row) => (
-              <StyledTableRow key={row.name}>
-                <StyledTableCell component="th" scope="row">
-                  {row.name}
-                </StyledTableCell>
-                <StyledTableCell align="left">
-                  <ul>
-                    <li>{row.calories}</li>
-                  </ul>
-                </StyledTableCell>
-                <StyledTableCell align="left">{row.fat}</StyledTableCell>
-                <StyledTableCell align="left">{row.carbs}</StyledTableCell>
-                <StyledTableCell align="left">{row.protein}</StyledTableCell>
-              </StyledTableRow>
-            ))}
+          <TableBody style={{ backgroundColor: "white", height: "100px" }}>
+            <StyledTableCell className="container">
+              <ul>
+                <li>
+                  Explain the key concepts of the Software Design process and
+                  demonstrate how the essential design principles are applied
+                  within it
+                </li>
+                <li>
+                  Explain the key concepts of the Software Design process and
+                  demonstrate how the essential design principles are applied
+                  within it
+                </li>
+              </ul>
+            </StyledTableCell>
+            <StyledTableCell className="container">
+              <ul>
+                <li>
+                  Explain the key concepts of the Software Design process and
+                  demonstrate how the essential design principles are applied
+                  within it
+                </li>
+              </ul>
+            </StyledTableCell>
+            <StyledTableCell className="container">
+              <ul>
+                <li>
+                  Explain the key concepts of the Software Design process and
+                  demonstrate how the essential design principles are applied
+                  within it
+                </li>
+                <li>
+                  Explain the key concepts of the Software Design process and
+                  demonstrate how the essential design principles are applied
+                  within it
+                </li>
+              </ul>
+            </StyledTableCell>
+            <StyledTableCell className="container">
+              <ul>
+                <li>
+                  Explain the key concepts of the Software Design process and
+                  demonstrate how the essential design principles are applied
+                  within it
+                </li>
+              </ul>
+            </StyledTableCell>
+            <StyledTableCell className="container">
+              <ul>
+                <li>
+                  Explain the key concepts of the Software Design process and
+                  demonstrate how the essential design principles are applied
+                  within it
+                </li>
+                <li>
+                  Explain the key concepts of the Software Design process and
+                  demonstrate how the essential design principles are applied
+                  within it
+                </li>
+              </ul>
+            </StyledTableCell>
           </TableBody>
         </Table>
       </TableContainer>
     </div>
   );
 }
+
+//  ! way to find max number lenght of an array inside an object
+// const obj = [
+//   { employees: ["e1", "e2", "e3", "f4"] },
+//   { employees: ["e4", "e5", "e6"] },
+//   { employees: ["e7", "e8", "e9"] },
+//   { employees: ["e10", "e11", "e12"] },
+//   { employees: ["e13", "e14", "e15"] },
+// ];
+// const arr = Object.values(obj)
+//   .map((obj) => Object.values(obj))
+//   .map((a) => a[0].length);
+// console.log(arr);
+// const max = arr.reduce((a, b) => Math.max(a, b), -Infinity);
+// console.log(max);
+
+// <TableBody>
+//             {rows.map((row) => (
+//               <StyledTableRow >
+//                 <StyledTableCell align="left">{row.topics}</StyledTableCell>
+//                 <StyledTableCell align="left">
+//                   <ul>
+//                     <li>{row.ILOs}</li>
+//                   </ul>
+//                 </StyledTableCell>
+//                 <StyledTableCell align="left">{row.TLAs}</StyledTableCell>
+//                 <StyledTableCell align="left">{row.ATs}</StyledTableCell>
+//                 <StyledTableCell align="left">{row.Remarks}</StyledTableCell>
+//               </StyledTableRow>
+//             ))}
+//           </TableBody>
